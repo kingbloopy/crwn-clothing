acd20ee05e6fc482df871f47c7d4008c79e8449b
Maggie <maggie.mcdonald@verbalplusvisual.com>
2022-12-08 08:39:46 -0800

    created auth page and styled

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -58,8 +58,8 @@ const SignUpForm = () => {
 
   return (
     <div className="sign-up-container">
-      <h2>Don't have an account?</h2>
-      <span>Sign up with your email and password</span>
+      <h2>New Here?</h2>
+      <span>Create an account with your email and password.</span>
       <form onSubmit={handleSubmit}>
 
         <FormInput
---
+++ src/routes/authentication/authentication.jsx
@@ -0,0 +1,15 @@
+import SignUpForm from "../../components/sign-up/sign-up";
+import SignInForm from "../../components/sign-in/sign-in";
+import './authentication.scss';
+
+const Authentication = () => {
+
+  return (
+    <div className="authentication-container">
+      <SignInForm/>
+      <SignUpForm/>
+    </div>
+  );
+}
+
+export default Authentication;
